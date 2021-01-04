7f2c6453d85c87702c02ba2712391fe3d8ae9da1
Iago <iago.gc88@gmail.com>
2021-01-04 22:30:41 +0100

    MILESTONE3
    Fibonacci

--- Program.cs
+++ Program.cs
@@ -7,9 +7,12 @@ using System.Threading.Tasks;
 namespace NET_M3 {
     class Program {
         static void Main(string[] args) {
-            MLST2 e2 = new MLST2();
-            e2.init();
-            
+            //MLST2 e2 = new MLST2();
+            //e2.init();
+
+            MLST3 e3 = new MLST3();
+            e3.Fibonacci(5);
+
             Console.ReadKey();
         }
     }
