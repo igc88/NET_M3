3c914f722e515d6cb353d9b101db2d61193c5539
Iago <iago.gc88@gmail.com>
2021-01-04 12:59:00 +0100

    MILESTONE1
    Array ciudades

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_M3 {
+    class Program {
+        static void Main(string[] args) {
+            MLST1 e1 = new MLST1();
+            e1.fase1();
+            
+            Console.ReadKey();
+        }
+    }
+}
